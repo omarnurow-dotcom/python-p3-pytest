cb2b7490338b80c5d4f99349b443b8e2c6d5b392
Your Name <your.email@example.com>
2025-11-29 12:51:25 +0300

    Implement return_string() and interpolate_string()

--- lib/string_functions.py
+++ lib/string_functions.py
@@ -1,6 +1,18 @@
-def return_string():
-    return ''
+# string_functions.py
 
-def interpolate_string(s):
-    return f'Hello, {s}!'
+def return_string():
+    """
+    This function returns a string.
+    """
+    return "Hello, World!"
+
+def interpolate_string(base_string, insert_string):
+    """
+    This function takes two strings and inserts insert_string into base_string.
+    
+    Example:
+    interpolate_string("I love {}", "Python") -> "I love Python"
+    """
+    return base_string.format(insert_string)
+
     
